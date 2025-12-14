747aa964ad4007b8efa3ef71421ffdd029331671
hasunwoo <whs1999kr@naver.com>
2025-12-14 16:57:45 +0900

    4

--- modules/base/boot.nix
+++ modules/base/boot.nix
@@ -3,7 +3,17 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.efi.efiSysMountPoint = "/boot/efi";
-    boot.kernelParams = [ "rootflags=subvol=@root" ];
+
+    boot.initrd.availableKernelModules = [
+        "ahci"
+        "sd_mod"
+        "usb_storage"
+        "nvme"
+    ];
+
+    boot.initrd.kernelModules = [
+        "btrfs"
+    ];
 
     boot.loader.timeout = 3;
 }
