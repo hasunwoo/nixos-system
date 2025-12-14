575f7efd2109ef262180e438dd7c8771c89da439
hasunwoo <whs1999kr@naver.com>
2025-12-14 22:58:11 +0900

    5

--- modules/base/boot.nix
+++ modules/base/boot.nix
@@ -5,14 +5,36 @@
     boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
     boot.initrd.availableKernelModules = [
+        # 공통 스토리지
         "ahci"
-        "sd_mod"
-        "usb_storage"
         "nvme"
+        "sd_mod"
+        "sr_mod"
+        "usb_storage"
+
+        # Device mapper
+        "dm_mod"
+        "dm_crypt"
+
+        # Virt (KVM/QEMU)
+        "virtio_pci"
+        "virtio_blk"
+        "virtio_scsi"
+        "virtio_net"
+
+        # Hyper-V
+        "hv_vmbus"
+        "hv_storvsc"
+
+        # VMware
+        "vmw_pvscsi"
     ];
 
-    boot.initrd.kernelModules = [
+    boot.initrd.supportedFilesystems = [
+        "ext4"
         "btrfs"
+        "vfat"
+        "xfs"
     ];
 
     boot.loader.timeout = 3;
