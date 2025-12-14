49a0b0ebc1d3cf08228b9be8f5a4e8c4ff8670f0
hasunwoo <whs1999kr@naver.com>
2025-12-14 16:39:56 +0900

    2

--- modules/base/boot.nix
+++ modules/base/boot.nix
@@ -3,6 +3,7 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.efi.efiSysMountPoint = "/boot/efi";
+    boot.kernelParams = [ "rootflags=subvol=@root" ];
 
     boot.loader.timeout = 3;
 }
