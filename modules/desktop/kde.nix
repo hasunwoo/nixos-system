ae2cf02f56d967c114af6dc356856a08dd683b03
hasunwoo <whs1999kr@naver.com>
2025-12-18 01:06:08 +0900

    d

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -1,38 +1,41 @@
 { config, pkgs, lib, ... }:
 {
-    config = {
-        # XDG / DBus
-        services.dbus.enable = true;
+    config = lib.mkMerge [
+        {
+            # XDG / DBus
+            services.dbus.enable = true;
 
-        # Display Manager
-        services.displayManager.sddm.enable = true;
-        services.displayManager.sddm.wayland.enable = true;
+            # Display Manager
+            services.displayManager.sddm.enable = true;
+            services.displayManager.sddm.wayland.enable = true;
 
-        # KDE Plasma (Wayland)
-        services.desktopManager.plasma6.enable = true;
+            # KDE Plasma (Wayland)
+            services.desktopManager.plasma6.enable = true;
 
-        # Wayland 권장 옵션
-        programs.xwayland.enable = true;
+            # Wayland 권장 옵션
+            programs.xwayland.enable = true;
 
-        # 폰트 (선택)
-        fonts.packages = with pkgs; [
-            noto-fonts
-            noto-fonts-cjk-sans
-            noto-fonts-emoji
-        ];
+            # 폰트 (선택)
+            fonts.packages = with pkgs; [
+                noto-fonts
+                noto-fonts-cjk-sans
+                noto-fonts-emoji
+            ];
 
-    } // lib.mkIf config.my.services.flatpak.enable {
-        # xdg
-        xdg.portal.enable = true;
-        xdg.portal.extraPortals = [
-            pkgs.xdg-desktop-portal-kde
-        ];
-        xdg.portal.config.common.default = "kde";
+        }
+        (lib.mkIf config.my.services.flatpak.enable {
+            # xdg
+            xdg.portal.enable = true;
+            xdg.portal.extraPortals = [
+                pkgs.xdg-desktop-portal-kde
+            ];
+            xdg.portal.config.common.default = "kde";
 
-        # discover app
-        environment.systemPackages = with pkgs; [
-            kdePackages.discover
-        ];
-    };
+            # discover app
+            environment.systemPackages = with pkgs; [
+                kdePackages.discover
+            ];
+        })
+    ]
 }
 
