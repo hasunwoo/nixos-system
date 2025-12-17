52130a380c78e85384438034dbd202c6f95fe599
hasunwoo <whs1999kr@naver.com>
2025-12-18 00:37:25 +0900

    s

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -1,32 +1,31 @@
 { config, pkgs, lib, ... }:
 {
-    # XDG / DBus
-    services.dbus.enable = true;
+    config = {
+        # XDG / DBus
+        services.dbus.enable = true;
 
-    # Display Manager
-    services.displayManager.sddm.enable = true;
-    services.displayManager.sddm.wayland.enable = true;
+        # Display Manager
+        services.displayManager.sddm.enable = true;
+        services.displayManager.sddm.wayland.enable = true;
 
-    # KDE Plasma (Wayland)
-    services.desktopManager.plasma6.enable = true;
+        # KDE Plasma (Wayland)
+        services.desktopManager.plasma6.enable = true;
 
-    # Wayland 권장 옵션
-    programs.xwayland.enable = true;
+        # Wayland 권장 옵션
+        programs.xwayland.enable = true;
 
-    # 폰트 (선택)
-    fonts.packages = with pkgs; [
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-emoji
-    ];
+        # 폰트 (선택)
+        fonts.packages = with pkgs; [
+            noto-fonts
+            noto-fonts-cjk-sans
+            noto-fonts-emoji
+        ];
 
-    # xdg
-    xdg.portal.extraPortals = [
-        pkgs.xdg-desktop-portal-kde
-    ];
-
-    # discover app
-    config = lib.mkIf config.my.services.flatpak.enable {
+        # xdg
+        xdg.portal.extraPortals = [
+            pkgs.xdg-desktop-portal-kde
+        ];
+    } // lib.mkIf config.my.services.flatpak.enable {
         environment.systemPackages = with pkgs; [
             kdePackages.discover
             kdePackages.discover-flatpak
